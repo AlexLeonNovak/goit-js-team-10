b50259f76255e15c23196cbaae760fb0af98f44d
Alex Novak <alexleonnovak@gmail.com>
2021-05-11 15:36:30 +0300

    prepare api service

--- src/index.js
+++ src/index.js
@@ -2,4 +2,9 @@ import "./sass/main.scss";
 //import 'material-design-icons/iconfont/material-icons.css';
 
 import ApiService from './js/apiService.js'
-import cardTpl from './templates/template-card.hbs';
+//import cardTpl from './templates/template-card.hbs';
+
+
+const api = new ApiService();
+
+console.log(api.fetchEvent());
--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,38 +1,79 @@
-
 const BASE_URL = 'https://app.ticketmaster.com/discovery/v2/';
 const API_KEY = 'bOxDzXD7U4DYwWKdKJpCszoGXxMX0Go3';
 
 export default class ApiService {
-    constructor() {
-        this.searchQuery = '';
-        this.page = 1;
+  constructor() {
+    this._searchQuery = '';
+    this._page = 1;
+    this._perPage = 20;
+    this._countryCode = '';
+  }
 
-     }
-    // fetchEvent() {
-    // return fetch(`${BASE_URL}events.json?keyword=${this.searchQuery}&source=universe&page=${this.page}&size=20&apikey=${API_KEY}`)
-    //     .then(response =>
-    // response.json(),
-    // ).then(({ hits }) => {
-        
-    //     this.nextPage();
-        
-    //     return hits;
-    // })
-    //     .catch(errorNotice);
-    // }
-    // nextPage() {
-    //     this.page += 1;
-    // }
+  get searchQuery() {
+    return this._searchQuery;
+  }
 
-    // resetPage() {
-    //     this.page = 1;
-    // }
-    // get query() {
-    //     return this.searchQuery;
-    // }
-    // set query(newQuery) {
-    //     this.searchQuery = newQuery;
-    // }
+  set searchQuery(query) {
+    this._page = 1;
+    this._searchQuery = query;
+  }
+
+  get countryCode() {
+    return this._countryCode;
+  }
+
+  set countryCode(countryCode) {
+    this._countryCode = countryCode;
+  }
+
+  set page(page) {
+    this._page = page;
+  }
+
+
+  fetchEvent() {
+    const params = {
+      key: API_KEY,
+      page: this._page,
+      per_page: this._perPage,
+    };
+
+    if (this._searchQuery.trim().length){
+      params.keyword = this._searchQuery.trim();
+    }
+
+    if (this._countryCode.trim().length) {
+      params.countryCode = this._countryCode.trim();
+    }
+
+    const headers = {
+      'Access-Control-Allow-Origin': '*'
+    }
+
+    return fetch(`${BASE_URL}events.json?${this.buildParamString(params)}`, {
+      headers
+    })
+      .then(response => response.json());
+  }
+
+  fetchEventDetail(id) {
+    const params = {
+      key: API_KEY
+    };
+
+    return fetch(`${BASE_URL}events/${id}?${this.buildParamString(params)}`)
+      .then(response => response.json());
+  }
+
+  buildParamString(params){
+    let str = [];
+    for (let key in params) {
+      if (params.hasOwnProperty(key)) {
+        str.push(`${key}=${params[key]}`);
+      }
+    }
+    return str.join('&');
+  }
 }
 
 
