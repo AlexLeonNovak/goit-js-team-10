50a6e370ac413afef9a83b5e3fee900ee9512394
Alex Novak <alexleonnovak@gmail.com>
2021-05-11 23:07:21 +0300

    render cards

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
-import "./sass/main.scss";
+import './sass/main.scss';
 //import 'material-design-icons/iconfont/material-icons.css';
+import ApiService from './services/api.service';
+import cardTpl from './templates/template-card.hbs';
+import { eventAdapter } from './utils/event-adapter';
 
-import ApiService from './services/api.service'
-//import cardTpl from './templates/template-card.hbs';
-
+const refs = {
+  eventList: document.querySelector('.event-list')
+}
 
 const api = new ApiService();
 
-api.fetchEvents().then(console.log);
-api.fetchEventDetail("1kk8v94-GA5YE-w").then(console.log);
+api.fetchEvents()
+  .then(({ _embedded }) => {
+    buildCards(_embedded);
+  })
+
+// api.fetchEventDetail("1kk8v94-GA5YE-w").then(console.log);
+
+
+function buildCards({events}) {
+  console.log(events);
+  refs.eventList.innerHTML = cardTpl(events.map(eventAdapter));
+}
