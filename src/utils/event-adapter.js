f081f2fca1a1ae3b2d686c606d4b8d0b6c358bb6
Alex Novak <alexleonnovak@gmail.com>
2021-05-16 22:33:34 +0300

    fix modal hbs

--- src/utils/event-adapter.js
+++ src/utils/event-adapter.js
@@ -1,9 +1,17 @@
 export const eventAdapter = event => ({
   id: event.id,
-  image: event.images[0].url,
+  image: {
+    small: event.images.find(img => {
+      return img.height === 360 && img.width === 640
+    }).url,
+    big: event.images.find(img => {
+      return img.height === 639 && img.width === 1136
+    }).url,
+  },
   name: event.name,
   date: event.dates.start.dateTime,
   place: event._embedded.venues[0].name,
   city: event._embedded.venues[0].city.name,
   country: event._embedded.venues[0].country.name,
+  prices: event.priceRanges
 });
